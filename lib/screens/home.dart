3786727180a7fcd0303284fbbdb6b232c73c9d94
ahmed adel <ahmedadel123422@gmail.com>
2025-03-24 00:14:15 +0200

    commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(
+          "Counter Page",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
